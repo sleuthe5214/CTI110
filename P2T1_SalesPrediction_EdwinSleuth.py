0829c82c220f2e92a10daa91b8bbb91a3d59d8cf
sleuthe5214 <43421194+sleuthe5214@users.noreply.github.com>
2018-09-23 23:04:12 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_EdwinSleuth.py
@@ -0,0 +1,15 @@
+#Getting the Sales projection for a company
+#23 September 2018
+#CTI-110 P2T1 - Sales Prediction
+#Edwin Sleuth
+
+# Get the projected total sales.
+total_sales= float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit= total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+                                 
